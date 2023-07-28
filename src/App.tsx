f5fb19726a1c0b433224666f2937f7ba4886eda3
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-07-28 14:47:16 +0300

    style: add adaptive rules for cards container and content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import * as TimSort from 'timsort';
 import { launches } from '../__mocks__/launches';
 import { Card } from './components/Card';
+
+const mockData = [...launches];
+
+TimSort.sort(mockData, (a, b) => b.date_unix - a.date_unix);
 
 function App() {
   return (
     <div className="bg-stone-100 w-full h-screen overflow-auto">
-      <div className="w-full max-w-6xl mx-auto flex flex-wrap gap-6 border-gray-100 my-6 justify-center">
-        {launches.map((l) => (
+      <div className="w-full max-w-6xl mx-auto flex flex-wrap lg:gap-6 lg:my-6 justify-center">
+        {mockData.map((l) => (
           <Card data={l} key={l.id} />
         ))}
       </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,11 +9,16 @@ function Card({ data }: Props) {
   const { name, details, date_utc: date, links } = data;
   const { small } = links.patch;
   return (
-    <div className="w-64 rounded border border-gray-300 p-4 flex flex-col gap-3">
-      <h1 className="font-semibold font-serif text-xl">{name}</h1>
-      <p className="max-h-48 overflow-auto text-sm">{details}</p>
-      <img className="mt-auto mx-auto p-4" src={small} alt="" srcSet="" />
-      <p className="font-mono text-xs ml-auto">{dayjs(date).format('DD/MM/YYYY')}</p>
+    <div className="lg:max-w-[500px] lg:border w-full rounded border-gray-300 p-4 flex flex-col gap-3 group">
+      <div className="flex justify-between flex-col sm:flex-row gap-3">
+        <div>
+          <h1 className="font-semibold font-serif text-xl mb-3">{name}</h1>
+          <p className="max-h-64 overflow-auto text-sm">{details}</p>
+        </div>
+        <img className="h-48 sm:h-36 pl-2 pb-2 self-center sm:self-start" src={small} alt="" srcSet="" />
+      </div>
+      <p className="font-mono text-xs ml-auto mt-auto">{dayjs(date).format('DD/MM/YYYY')}</p>
+      <div className="lg:hidden w-full border-b border-gray-300" />
     </div>
   );
 }
