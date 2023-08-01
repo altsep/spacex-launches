c321a10744f184e517b97436d7444c7d1c5485bf
Alexei Arushanov <3k4ffb8dj@mozmail.com>
2023-08-01 19:14:45 +0300

    refactor: use a separate fn to transform docs in msw handlers

--- __mocks__/api/handlers.ts
+++ __mocks__/api/handlers.ts
@@ -1,24 +1,29 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { rest } from 'msw';
-import { LaunchesQueryArg } from '../../src/models/launchesQueryArg.model';
+import { LaunchesQueryArg, QueryOptions } from '../../src/models/launchesQueryArg.model';
 import { API_BASE_URL, ApiPath } from '../../src/utils/constants';
 import { sortByDate } from '../../src/utils/helpers/sorting.helpers';
 import placeholderImg from '../assets/rocket_placeholder.png';
 import { mockLaunchesQueryRes } from '../launchesQueryRes.mock';
 
+const transformDocs = (options: QueryOptions) => {
+  const startIndex = (options.page - 1) * options.limit;
+  const endIndex = startIndex + options.limit;
+  const sortMode = options.sort[0] === '-' ? 'desc' : 'asc';
+  const docs = sortByDate(mockLaunchesQueryRes.docs, sortMode).slice(startIndex, endIndex);
+  return docs;
+};
+
 export const handlers = [
   rest.post(`${API_BASE_URL}${ApiPath.launches}/query`, async (req, res, ctx) => {
     const { options }: LaunchesQueryArg = await req.json();
-    const startIndex = (options.page - 1) * options.limit;
-    const endIndex = startIndex + options.limit;
-    const sortMode = options.sort[0] === '-' ? 'desc' : 'asc';
-    const docs = sortByDate(mockLaunchesQueryRes.docs, sortMode).slice(startIndex, endIndex);
+    const { page } = options;
     const data = {
       ...mockLaunchesQueryRes,
-      page: options.page,
-      hasNextPage: options.page < mockLaunchesQueryRes.totalPages,
-      nextPage: options.page + 1,
-      docs,
+      page,
+      hasNextPage: page < mockLaunchesQueryRes.totalPages,
+      nextPage: page + 1,
+      docs: transformDocs(options),
     };
     return res(ctx.status(200), ctx.json(data), ctx.delay(30));
   }),
